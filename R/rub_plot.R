a433783fc43e8eed4f7bae2090ba7450ec21f32d
Richard Meyer-Eppler <richard.meyer-eppler@uv.ruhr-uni-bochum.de>
2022-01-12 14:48:13 +0100

    Created separate file for example of rub_plot_type_1

--- R/rub_plot.R
+++ R/rub_plot.R
@@ -137,31 +137,7 @@ plot_discrete_palette <- function(colors_n)  {
 #' @export
 #' @importFrom rlang .data
 #'
-#' @examples
-#' # Create test values for all three mandatory variables (x_var, y_var,
-#' # fill_var).
-#' df_t1_ex1 <- tibble::tribble(
-#'    ~term, ~students, ~degree,
-#'    "Spring '13", 120, "Bachelor 1-Subject",
-#'    "Spring '14", 105, "Bachelor 1-Subject",
-#'    "Spring '15", 124, "Bachelor 1-Subject",
-#'    "Spring '16", 114, "Bachelor 1-Subject",
-#'    "Spring '17", 122, "Bachelor 1-Subject",
-#'    "Spring '13", 121, "Master 1-Subject",
-#'    "Spring '14", 129, "Master 1-Subject",
-#'    "Spring '15", 122, "Master 1-Subject",
-#'    "Spring '16", 168, "Master 1-Subject",
-#'    "Spring '17", 7, "Master 1-Subject",
-#' )
-#'
-#' # The data source is df_t1_ex1, x_var is mapped to term, y_var to students,
-#' # and the fill_var to degree.
-#' rub_plot_type_1(
-#'    df = df_t1_ex1,
-#'    x_var = term,
-#'    y_var = students,
-#'    fill_var = degree
-#' )
+#' @example inst/examples/rub_plot_type_1.R
 rub_plot_type_1 <- function(df, x_var,
                            y_var, y_axis_label = "",
                            fill_var, fill_reverse = FALSE,
